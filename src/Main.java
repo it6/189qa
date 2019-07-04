7ea72234df34b842bad7e4e047ba3822536bbcbb
Surya Kotamraju <kotamraju@me.com>
2019-07-03 21:48:29 -0400

    update idea project

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-public class Main {
+import java.util.*;
 
+public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
     }
 }
 
 class Solution {
+
 }
